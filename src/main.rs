d4b94cde51b94b463ff2c3f2189b4fefa3129e8e
Livable8209 <136758278+Livable8209@users.noreply.github.com>
2025-03-14 12:25:48 -0400

    beginning discovery

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@
 // then renames those files to the collection of name randomly
 // (this is made for tetrio plus but it should just work fine for any other files)
 
+use std::error::Error;
+use std::fs::{self};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn Error>> {
+    let stuff = fs::read_dir(".")?;
+    for dirres in stuff {
+        let unwrapped = dirres?;
+        println!("{:?}", unwrapped.path());
+    }
+    Ok(())
 }
