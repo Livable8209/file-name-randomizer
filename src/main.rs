f9f3cf53c3d684effe8b9b2f003600d064d573a3
Livable8209 <136758278+Livable8209@users.noreply.github.com>
2025-03-14 11:16:51 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// tetrio plus sound randomizer
+// takes a directory of files, collects all the names
+// then renames those files to the collection of name randomly
+// (this is made for tetrio plus but it should just work fine for any other files)
+
+
+fn main() {
+    println!("Hello, world!");
+}
