2c7eb50c3e58dfc83cf94690a1650a14dfbc4e25
Livable8209 <136758278+Livable8209@users.noreply.github.com>
2025-03-16 10:59:34 -0400

    handle unknown path error and much more

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,29 @@
 // then renames those files to the collection of name randomly
 // (this is made for tetrio plus but it should just work fine for any other files)
 
+use rand::prelude::*;
 use std::error::Error;
 use std::path::Path;
-use std::process::exit;
 use std::{env, fs, io};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut rng = rand::rng();
+
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
 
-    let path = args.first().unwrap(); //this will error if no path is given :(
+    let default_path = String::from(".");
+    let path = args.first().unwrap_or(&default_path);
 
     let input_directory = match fs::read_dir(path) {
         Ok(dir) => dir,
         Err(err) => {
-            println!("Error hit, exiting. {}", err);
-            exit(1);
+            panic!("Error hit, exiting. {}", err);
         } // if we ever hit this, we better stop
     };
     let input_directory = input_directory
         .map(|result| result.map(|path| path.path()))
         .collect::<Result<Vec<_>, io::Error>>();
-
-    let files = input_directory.unwrap(); // if this errors, something TERRIBLE has happened
-
-    for file in files {
-        println!("{:?}", file.file_name());
-    }
 
     if !fs::exists("randomized_sounds")? {
         println!("Making randomized_sounds directory");
@@ -43,6 +39,34 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let randomized_dir = Path::new("randomized_sounds");
 
-    println!("{}", randomized_dir.exists());
+    let files = input_directory.unwrap(); // if this errors, something TERRIBLE has happened
+    let mut file_names: Vec<String> = Vec::with_capacity(files.len());
+
+    for file in &files {
+        file_names.push(String::from(file.file_name().unwrap().to_string_lossy())); // wow that is just terrible and bad
+    }
+
+    file_names.shuffle(&mut rng);
+
+    for file in &files {
+        let end_path = format!(
+            "{}/{}",
+            randomized_dir.to_string_lossy(),
+            file.file_name()
+                .expect("unwrapped .. somehow?")
+                .to_string_lossy()
+        );
+
+        let file_copy = fs::copy(file, end_path);
+        if file_copy.is_err() {
+            println!(
+                "Hit err on trying to write file! {}",
+                file_copy.err().unwrap()
+            )
+        }
+    }
+
+    dbg!(file_names);
+
     Ok(())
 }
