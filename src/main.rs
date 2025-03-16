60786f22a748770f358727843e55c8a1970425fb
Livable8209 <136758278+Livable8209@users.noreply.github.com>
2025-03-16 09:22:08 -0400

    more handling for reading in dirs

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,45 @@
 // (this is made for tetrio plus but it should just work fine for any other files)
 
 use std::error::Error;
-use std::{fs, io};
+use std::path::Path;
+use std::process::exit;
+use std::{env, fs, io};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let stuff = match fs::read_dir("test_files") {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+
+    let path = args.first().unwrap(); //this will error if no path is given :(
+
+    let input_directory = match fs::read_dir(path) {
         Ok(dir) => dir,
-        Err(err) => panic!("We hit an error! {}", err), // if we ever hit this, we better stop
+        Err(err) => {
+            println!("Error hit, exiting. {}", err);
+            exit(1);
+        } // if we ever hit this, we better stop
     };
-    let stuff = stuff
+    let input_directory = input_directory
         .map(|result| result.map(|path| path.path()))
         .collect::<Result<Vec<_>, io::Error>>();
 
-    let files = stuff.unwrap();
+    let files = input_directory.unwrap(); // if this errors, something TERRIBLE has happened
 
     for file in files {
         println!("{:?}", file.file_name());
     }
+
+    if !fs::exists("randomized_sounds")? {
+        println!("Making randomized_sounds directory");
+        let res = fs::create_dir("randomized_sounds");
+        if res.is_err() {
+            println!("Caught error: {:?}", res.err());
+        }
+    } else {
+        println!("randomized_sounds already exists");
+    }
+
+    let randomized_dir = Path::new("randomized_sounds");
+
+    println!("{}", randomized_dir.exists());
     Ok(())
 }
